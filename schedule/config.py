b611870fa016e72583390200efb317b2c31af930
Ruslan Bel'kov <ruslan.belckov@yandex.ru>
2023-02-05 12:47:10 +0300

    feat(schedule) Refactor output and skip "Physical education" classes
    now output json file - academic.json:
    "filters": ["course"],
    "calendars": [
    {
    "name": str,
    "file": path to .ics file,
    "course": key for course filter
    }
    ]

--- schedule/config.py
+++ schedule/config.py
@@ -9,16 +9,19 @@ CONFIG_PATH = PARSER_PATH / "config.json"
 
 
 @dataclass
-class ParserConfig:
+class AcademicParserConfig:
     SPREADSHEET_ID: str
     TARGET_RANGES: list[str]
     TARGET_SHEET_TITLES: list[str]
     RECURRENCE: list[dict]
     SAVE_PATH: str
 
+    OUTPUT_JSON_NAME: str = "academic.json"
+
     TARGET_ID: int = 0
 
     CREDENTIALS_PATH: str = "credentials.json"
+
     API_SCOPES = ['https://www.googleapis.com/auth/spreadsheets.readonly']
     TIMEZONE = 'Europe/Moscow'
     TIMEZONE_DELTA = '+03:00'
@@ -31,11 +34,17 @@ class ParserConfig:
             'SATURDAY',
             'SUNDAY']
 
+    IGNORING_CLASSES = ["Physical Education"]
+
 
 with open(CONFIG_PATH, 'r') as f:
     config_dict = json.load(f)
+    academic_config_dict = config_dict["academic"]
+    sport_config_dict = config_dict["sport"]
+    elective_config_dict = config_dict["elective"]
+    dormitory_config_dict = config_dict["dormitory"]
 
-config: ParserConfig = parse_obj_as(ParserConfig, config_dict)
+config: AcademicParserConfig = parse_obj_as(AcademicParserConfig, academic_config_dict)
 
 __all__ = ['config', 'PARSER_PATH']
 
