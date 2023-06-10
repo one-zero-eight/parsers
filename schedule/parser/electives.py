a0766eebd55e7a87c1bcd689b9012ed60ea0bd2e
mazzz3r <i@mazzz3r.ru>
2023-06-10 20:00:08 +0300

    docs(schedule/electives): Add docs
    [skip ci]

--- schedule/parser/electives.py
+++ schedule/parser/electives.py
@@ -30,11 +30,19 @@ class ElectiveEvent(BaseModel):
     group: Optional[str] = None
 
     def __hash__(self):
-        return hash((self.elective.alias, self.start, self.end, self.location, self.event_type,
-                     self.group))
+        return hash(
+            (
+                self.elective.alias,
+                self.start,
+                self.end,
+                self.location,
+                self.event_type,
+                self.group,
+            )
+        )
 
     def get_uid(self) -> str:
-        return '%x@innopolis.ru' % abs(hash(self))
+        return "%x@innopolis.ru" % abs(hash(self))
 
 
 class ElectiveParser:
@@ -43,30 +51,29 @@ class ElectiveParser:
     logger = logging.getLogger(__name__ + "." + "Parser")
 
     def __init__(self):
-
         self.credentials = get_credentials(
             Path(config.CREDENTIALS_PATH),
             PARSER_PATH / "token.json",
-            scopes=config.API_SCOPES
+            scopes=config.API_SCOPES,
         )
         self.spreadsheets = connect_spreadsheets(self.credentials)
 
     def get_clear_df(
-            self,
-            spreadsheet_id: str,
-            target_range: str,
-            target_title: str
+        self, spreadsheet_id: str, target_range: str, target_title: str
     ) -> pd.DataFrame:
-        """ Get data from Google Sheets and return it as a DataFrame with merged
-        cells and empty cells in the course row filled by left value. """
+        """Get data from Google Sheets and return it as a DataFrame with merged
+        cells and empty cells in the course row filled by left value."""
 
         self.logger.debug("Getting dataframe from Google Sheets...")
-        self.logger.info(f"Retrieving data: {spreadsheet_id}/{target_title}-{target_range}")
+        self.logger.info(
+            f"Retrieving data: {spreadsheet_id}/{target_title}-{target_range}"
+        )
 
-        values = self.spreadsheets.values().get(  # type : ignore
-            spreadsheetId=spreadsheet_id,
-            range=target_range
-        ).execute()["values"]
+        values = (
+            self.spreadsheets.values()
+            .get(spreadsheetId=spreadsheet_id, range=target_range)  # type : ignore
+            .execute()["values"]
+        )
 
         df = pd.DataFrame(data=values)
         # strip all values
@@ -77,36 +84,40 @@ class ElectiveParser:
         spreadsheet = self.spreadsheets.get(
             spreadsheetId=config.SPREADSHEET_ID,
             ranges=[target_range],
-            includeGridData=False  # values already fetched
+            includeGridData=False,  # values already fetched
         ).execute()
         self.logger.info(
-            f"Spreadsheet {spreadsheet['properties']['title']} retrieved:" +
-            f"Sheets({len(spreadsheet['sheets'])}): " +
-            f"> {sheet['properties']['title']}"
-            for sheet in spreadsheet['sheets']
+            f"Spreadsheet {spreadsheet['properties']['title']} retrieved:"
+            + f"Sheets({len(spreadsheet['sheets'])}): "
+            + f"> {sheet['properties']['title']}"
+            for sheet in spreadsheet["sheets"]
         )
 
         # get target sheet
         target_sheet = None
-        for sheet in spreadsheet['sheets']:
-            if sheet['properties']['title'] == target_title:
+        for sheet in spreadsheet["sheets"]:
+            if sheet["properties"]["title"] == target_title:
                 target_sheet = sheet
                 break
 
         if target_sheet is None:
             raise ValueError(f"Target sheet {target_title} not found")
 
-        self.logger.info(f"Target sheet: {target_sheet['properties']['title']}" +
-                         f"> Sheet index: ({target_sheet['properties']['index']})")
+        self.logger.info(
+            f"Target sheet: {target_sheet['properties']['title']}"
+            + f"> Sheet index: ({target_sheet['properties']['index']})"
+        )
 
-        df.fillna('', inplace=True)
+        df.fillna("", inplace=True)
 
         self.logger.info("Cells merged")
         self.logger.info("Dataframe ready")
         return df
 
     @staticmethod
-    def parse_week_df(df: pd.DataFrame, electives: Collection[Elective]) -> list[ElectiveEvent]:
+    def parse_week_df(
+        df: pd.DataFrame, electives: Collection[Elective]
+    ) -> list[ElectiveEvent]:
         # for each cell in day column
         def process_cell(cell: str):
             #           "BDLD(lec) 312" ->
@@ -118,9 +129,7 @@ class ElectiveParser:
             if not cell:
                 return result
 
-            occurences = (cell
-                          .strip()
-                          .split('\n'))
+            occurences = cell.strip().split("\n")
 
             for line in occurences:
                 dct = {}
@@ -129,16 +138,16 @@ class ElectiveParser:
                 if r:
                     event_type = r.group(1)
                     event_type = event_type.replace(" ", "")
-                    line = line.replace(r.group(0), ' ')
-                    if event_type.startswith('lab'):
+                    line = line.replace(r.group(0), " ")
+                    if event_type.startswith("lab"):
                         if "/" in event_type:
-                            dct['group'] = event_type.split("/")[1]
-                        event_type = 'lab'
+                            dct["group"] = event_type.split("/")[1]
+                        event_type = "lab"
                     elif not event_type.startswith(("lec", "tut")):
-                        dct['group'] = event_type
+                        dct["group"] = event_type
                         event_type = None
 
-                    dct['event_type'] = event_type
+                    dct["event_type"] = event_type
 
                 parts = line.split()
 
@@ -148,17 +157,13 @@ class ElectiveParser:
                     event_name = parts[0]
                     event_location = None
 
-                elective = next(
-                    (e for e in electives
-                     if e.alias == event_name),
-                    None
-                )
+                elective = next((e for e in electives if e.alias == event_name), None)
 
                 if elective is None:
                     raise ValueError(f"Course not found: {event_name}")
 
-                dct['elective'] = elective
-                dct['location'] = event_location
+                dct["elective"] = elective
+                dct["location"] = event_location
 
                 result.append(dct)
             return result
@@ -183,7 +188,7 @@ class ElectiveParser:
         df.columns = day_row
         df = df.drop(day_row.name, axis=0)
 
-        days = [day for day in df.columns if day != '']
+        days = [day for day in df.columns if day != ""]
 
         for day in days:
             # copy column
@@ -192,12 +197,11 @@ class ElectiveParser:
             dtstart = dtstart.replace(year=datetime.now().year)
             # drop rows with empty cells
             day_df = day_df.dropna()
-            day_df = day_df[day_df != '']
+            day_df = day_df[day_df != ""]
             # for each cell in day column
             for timeslot, cell in day_df.items():
                 start_delta, end_delta = map(
-                    lambda t: datetime.strptime(t, '%H:%M').time(),
-                    timeslot.split("-")
+                    lambda t: datetime.strptime(t, "%H:%M").time(), timeslot.split("-")
                 )
                 cell_events = process_cell(cell)
                 event_start = datetime.combine(dtstart, start_delta)
@@ -205,15 +209,15 @@ class ElectiveParser:
 
                 for cell_event in cell_events:
                     event = ElectiveEvent(
-                        start=event_start,
-                        end=event_end,
-                        **cell_event
+                        start=event_start, end=event_end, **cell_event
                     )
                     events.append(event)
 
         return events
 
-    def parse_df(self, df: pd.DataFrame, electives: list[Elective]) -> list[ElectiveEvent]:
+    def parse_df(
+        self, df: pd.DataFrame, electives: list[Elective]
+    ) -> list[ElectiveEvent]:
         """Parse DataFrame to dict with separation by groups."""
 
         self.logger.debug("Parsing dataframe to separation by days|groups...")
@@ -228,10 +232,10 @@ class ElectiveParser:
 
         events = []
 
-        for (start_y, end_y) in pairwise(week_indexes + [max_x]):
+        for start_y, end_y in pairwise(week_indexes + [max_x]):
             week = df.iloc[start_y, 0]
             self.logger.info(f"> Week: {week}")
-            week_df = df.iloc[start_y: end_y, :]
+            week_df = df.iloc[start_y:end_y, :]
             week_events = self.parse_week_df(week_df, electives)
             events.extend(week_events)
 
@@ -239,7 +243,7 @@ class ElectiveParser:
 
 
 def convert_separation(
-        events: list[ElectiveEvent],
+    events: list[ElectiveEvent],
 ) -> dict[str, icalendar.Calendar]:
     output = defaultdict(lambda: icalendar.Calendar())
     # group events by Elective and group
@@ -255,14 +259,14 @@ def convert_separation(
         for event in events:
             event: ElectiveEvent
             vevent = icalendar.Event()
-            vevent['summary'] = elective.name
+            vevent["summary"] = elective.name
             if event.event_type is not None:
-                vevent['summary'] += f" ({event.event_type})"
-            vevent['dtstart'] = event.start.strftime("%Y%m%dT%H%M%S")
-            vevent['dtend'] = event.end.strftime("%Y%m%dT%H%M%S")
-            vevent['location'] = event.location
-            vevent['uid'] = event.get_uid()
-            vevent['categories'] = elective.name
+                vevent["summary"] += f" ({event.event_type})"
+            vevent["dtstart"] = event.start.strftime("%Y%m%dT%H%M%S")
+            vevent["dtend"] = event.end.strftime("%Y%m%dT%H%M%S")
+            vevent["location"] = event.location
+            vevent["uid"] = event.get_uid()
+            vevent["categories"] = elective.name
             desc = f"{elective.name}"
 
             if group is not None:
@@ -271,27 +275,29 @@ def convert_separation(
             if elective.instructor:
                 desc += f"\n{elective.instructor}"
 
-            if elective.type:
-                desc += f"\n{elective.type}"
+            if elective.elective_type:
+                desc += f"\n{elective.elective_type}"
 
-            vevent['description'] = desc
+            vevent["description"] = desc
             cal.add_component(vevent)
 
     return dict(output)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO)
 
     parser = ElectiveParser()
 
     calendars = {
-        "filters": [{
-            "title": "Elective type",
-            "alias": "elective_type",
-        }],
+        "filters": [
+            {
+                "title": "Elective type",
+                "alias": "elective_type",
+            }
+        ],
         "title": "Electives",
-        "calendars": []
+        "calendars": [],
     }
 
     for i in range(len(config.TARGET_SHEET_TITLES)):
@@ -300,35 +306,41 @@ if __name__ == '__main__':
         df = parser.get_clear_df(
             spreadsheet_id=config.SPREADSHEET_ID,
             target_title=sheet_title,
-            target_range=config.TARGET_RANGES[i]
+            target_range=config.TARGET_RANGES[i],
         )
 
         parsed = parser.parse_df(df, config.ELECTIVES)
         converted = convert_separation(parsed)
 
-        directory = PARSER_PATH / config.SAVE_PATH / sheet_title.replace("/", "-").replace(" ", "-")
+        directory = (
+            PARSER_PATH
+            / config.SAVE_PATH
+            / sheet_title.replace("/", "-").replace(" ", "-")
+        )
 
         directory.mkdir(parents=True, exist_ok=True)
 
         for elective_name, calendar in converted.items():
             # print
-            calendar['prodid'] = '-//one-zero-eight//InNoHassle Calendar'
-            calendar['version'] = '2.0'
-            calendar['x-wr-calname'] = elective_name
-            calendar['x-wr-caldesc'] = 'Generated by InNoHassle Calendar'
-            calendar['x-wr-timezone'] = config.TIMEZONE
+            calendar["prodid"] = "-//one-zero-eight//InNoHassle Calendar"
+            calendar["version"] = "2.0"
+            calendar["x-wr-calname"] = elective_name
+            calendar["x-wr-caldesc"] = "Generated by InNoHassle Calendar"
+            calendar["x-wr-timezone"] = config.TIMEZONE
 
             file_path = directory / f"{elective_name}.ics"
-            relative_directory = file_path.relative_to((PARSER_PATH / config.SAVE_JSON_PATH).parent)
+            relative_directory = file_path.relative_to(
+                (PARSER_PATH / config.SAVE_JSON_PATH).parent
+            )
             calendars["calendars"].append(
                 {
                     "name": elective_name,
                     "elective_type": config.TARGET_SHEET_TITLES[i],
-                    "file": relative_directory.as_posix()
+                    "file": relative_directory.as_posix(),
                 }
             )
 
-            with open(file_path, 'wb') as f:
+            with open(file_path, "wb") as f:
                 f.write(calendar.to_ical())
 
     # create a new .json file with information about calendar
