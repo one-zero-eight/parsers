1d4dcdc867c4337223ec27d277da831d8cfb46bf
Ruslan Bel'kov <ruslan.belckov@yandex.ru>
2023-10-16 19:12:09 +0300

    feat: update colors

--- schedule/config_base.py
+++ schedule/config_base.py
@@ -37,17 +37,29 @@ class VDayOfWeek(StrEnum):
 
 
 class CSS3Color(StrEnum):
-    BLUE_VIOLET = "blueviolet"
     BROWN = "brown"
+    CADETBLUE = "cadetblue"
     CHOCOLATE = "chocolate"
-    BURLYWOOD = "burlywood"
-    CORAL = "coral"
-    CRIMSON = "crimson"
-    DARK_BLUE = "darkblue"
+    DARK_CYAN = "darkcyan"
     DARK_GREEN = "darkgreen"
+    DARK_MAGENTA = "darkmagenta"
+    DARK_OLIVE_GREEN = "darkolivegreen"
+    DARK_RED = "darkred"
+    DARK_SLATE_BLUE = "darkslateblue"
+    DARK_SLATE_GRAY = "darkslategray"
+    DIM_GRAY = "dimgray"
     FIREBRICK = "firebrick"
+    FOREST_GREEN = "forestgreen"
+    GRAY = "gray"
+    INDIAN_RED = "indianred"
+    LIGHT_SLATE_GRAY = "lightslategray"
+    MAROON = "maroon"
+    MEDIUM_VIOLET_RED = "mediumvioletred"
+    MIDNIGHT_BLUE = "midnightblue"
     INDIGO = "indigo"
     REBECCA_PURPLE = "rebeccapurple"
+    SEA_GREEN = "seagreen"
+    TEAL = "teal"
 
     @classmethod
     def __len__(cls):
